2a49aa16b77ce69a8fac04400d657784e89b6544
aswath0001 <rsaswaths38@gmail.com>
2025-07-14 10:46:41 +0530

    first commit

---
+++ C3Q4.java
@@ -0,0 +1,9 @@
+public class C3Q4 {
+    public static void main(String[] args) {
+        for(int i= 0 ;i<=100;i++){
+            if(i % 3==0 && i%5==0){
+             System.out.println(i);
+            }
+        }
+    }
+}
