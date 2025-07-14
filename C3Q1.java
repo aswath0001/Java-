98c169c027559eb3302174ca5fbca05612c4ddcb
aswath0001 <rsaswaths38@gmail.com>
2025-07-14 10:34:06 +0530

    first commit

--- C3Q1.java
+++ C3Q1.java
@@ -1,3 +1,13 @@
+
+import java.util.Scanner;
+
 public class C3Q1 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        for(int i= A;i<=B;i++){
+          System.out.println(i);
+        }
+    }
 }
